bbd40cf7ead4de1a77aa8eae04647de0ed09b120
Renato De Giovanni <renato@cria.org.br>
2017-11-14 08:23:32 -0200

    Configuracao de log

--- website/settings.py
+++ website/settings.py
@@ -146,6 +146,30 @@ SITE_ID = 1
 # Django admin interface prefix
 MY_ADMIN_PREFIX = 'SET IN LOCAL SETTINGS'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+         'require_debug_false': {
+             '()': 'django.utils.log.RequireDebugFalse'
+         }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
+
 # TinyMCE configuration
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': 'table,paste',
