1865e2183c1c10fb9a0e7b4f22c1377b12f521dc
Renato De Giovanni <renato@cria.org.br>
2017-11-14 08:13:01 -0200

    Modo debug desativado

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'SET IN LOCAL SETTINGS'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['voluntarios.com.br']
 
