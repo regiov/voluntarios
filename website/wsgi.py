6f2c7d1cdd54d4f68d3f13a7c32e24d4755366ee
Renato De Giovanni <renato@cria.org.br>
2018-05-04 18:58:53 -0300

    Possibilidade de colocar aviso de manutencao no site

--- website/wsgi.py
+++ website/wsgi.py
@@ -9,8 +9,37 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+em_manutencao = False
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+def exibe_em_manutencao(environ, start_response):
 
-application = get_wsgi_application()
+    wsgi_dir = os.path.dirname(os.path.abspath(__file__))
+
+    template_manut_html = os.path.join(wsgi_dir, '..', 'vol', 'templates', 'em_manutencao.html')
+
+    if os.path.exists(template_manut_html):
+        response_headers = [('Content-type','text/html')]
+        import codecs
+        response = codecs.open(template_manut_html, 'r', 'latin1').read()
+    else:
+        response_headers = [('Content-type','text/plain')]
+        response = u'Site em manutenção... favor recarregar a página dentro de alguns minutos.'
+    
+    if environ['REQUEST_METHOD'] == 'GET':
+        status = '503 Service Unavailable'
+    else:
+        status = '405 Method Not Allowed'
+
+    start_response(status, response_headers)
+    return [response.encode('latin-1')]
+
+
+if em_manutencao:
+
+    application = exibe_em_manutencao
+
+else:
+
+    from django.core.wsgi import get_wsgi_application
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.settings")
+    application = get_wsgi_application()
