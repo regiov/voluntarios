14f7629fa6113cc685629d63b442031ea1331d4a
Renato De Giovanni <renato@cria.org.br>
2018-11-28 14:46:56 -0200

    Atualizacao na logica de delecao com novo esquema do allauth

--- vol/management/commands/cleanup.py
+++ vol/management/commands/cleanup.py
@@ -6,13 +6,19 @@ from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
 from django.utils import timezone
 
-from vol.models import Voluntario
+from allauth.account.models import EmailAddress
 
 class Command(BaseCommand):
-    help = u"Apaga voluntários que não confirmaram e-mail há mais de 3 meses."
+    help = u"Apaga usuários que não confirmaram e-mail há mais de 3 meses."
     usage_str = "Uso: ./manage.py cleanup"
 
     @transaction.atomic
     def handle(self, *args, **options):
 
-        result = Voluntario.objects.filter(aprovado__isnull=True, confirmado=False, data_cadastro__lte=timezone.now()-timedelta(days=90)).delete()
+        emails_antigos_nao_confirmados = EmailAddress.objects.filter(verified=False, user__date_joined__lte=timezone.now()-timedelta(days=90))
+        for email in emails_antigos_nao_confirmados:
+            # Verificamos se o usuário já cadastrou algo mais antes de apagá-lo.
+            # É uma forma de contornar o fato de ainda não termos controle de quando o usuário
+            # cadastrou o e-mail, uma vez que este pode ser alterado.
+            if not email.user.is_voluntario:
+                resultado = email.user.delete()
