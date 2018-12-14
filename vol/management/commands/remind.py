36dce0c699018eea056197836fd4604cae326a8e
Renato De Giovanni <renato@cria.org.br>
2018-12-14 18:10:08 -0200

    Novo comando pronto para uso

--- vol/management/commands/remind.py
+++ vol/management/commands/remind.py
@@ -4,9 +4,10 @@ from datetime import timedelta
 
 from django.core.management.base import BaseCommand, CommandError
 from django.db import transaction
+from django.db.models import Exists, OuterRef
 from django.utils import timezone
 
-from vol.models import Usuario
+from vol.models import Usuario, Voluntario
 from notification.models import Message, Event
 
 from notification.utils import notify_user_msg
@@ -18,14 +19,10 @@ class Command(BaseCommand):
     @transaction.atomic
     def handle(self, *args, **options):
 
-        usuarios = Usuario.objects.filter(link='voluntario_novo', date_joined__lte=timezone.now()-timedelta(days=7), date_joined__gte=timezone.now()-timedelta(days=30))
-
         msg = Message.objects.get(code='LEMBRETE_CADASTRO_VOLUNTARIO')
 
+        usuarios = Usuario.objects.annotate(sem_cadastro=~Exists(Voluntario.objects.filter(usuario=OuterRef('pk')))).annotate(sem_notificacao=~Exists(Event.objects.filter(user=OuterRef('pk'), message=msg))).filter(link='voluntario_novo', date_joined__lte=timezone.now()-timedelta(days=7), date_joined__gte=timezone.now()-timedelta(days=30), sem_cadastro=True, sem_notificacao=True, emailaddress__verified=True)
+
         for usuario in usuarios:
-            if not usuario.is_voluntario:
-                try:
-                    evt = Event.objects.get(user=usuario, message=msg)
-                except Message.DoesNotExist:
-                    notify_user_msg(usuario, msg)
+            notify_user_msg(usuario, msg)
 
