f15b92ecc3c0f674f2fd2202c6311e9132fe4758
regiov <renato@cria.org.br>
2023-12-27 15:19:58 -0300

    Generalização de script para gerenciar transições automáticas de status de processos seletivos

--- vol/management/commands/atualizar_processos_seletivos.py
+++ vol/management/commands/atualizar_processos_seletivos.py
@@ -8,8 +8,8 @@ from vol.models import ProcessoSeletivo, StatusProcessoSeletivo
 
 class Command(BaseCommand):
     '''Linha de comando para ser colocado para rodar no cron todos os dias logo após a meia-noite.'''
-    help = u"Encerra processos seletivos cujo limite de inscrições já passou mas que ainda se encontram ABERTO_A_INSCRICOES."
-    usage_str = "Uso: ./manage.py encerrar_processos_seletivos_vencidos"
+    help = u"Encerra processos seletivos cujo limite de inscrições já passou mas que ainda se encontram ABERTO_A_INSCRICOES, bem como abre para inscrições processos seletivos cujo início de inscrições já começõu mas que ainda se encontram AGUARDANDO_PUBLICACAO."
+    usage_str = "Uso: ./manage.py atualizar_processos_seletivos"
 
     @transaction.atomic
     def handle(self, *args, **options):
@@ -24,3 +24,14 @@ class Command(BaseCommand):
             i = i + 1
 
         self.stdout.write(self.style.NOTICE(str(i) + ' processo(s) seletivo(s) encerrado(s).'))
+
+        processos_nao_iniciados = ProcessoSeletivo.objects.filter(status=StatusProcessoSeletivo.AGUARDANDO_PUBLICACAO, inicio_inscricoes__lt=timezone.now())
+
+        i = 0
+
+        for processo in processos_nao_iniciados:
+            processo.publicar()
+            processo.save()
+            i = i + 1
+
+        self.stdout.write(self.style.NOTICE(str(i) + ' processo(s) seletivo(s) iniciado(s).'))
