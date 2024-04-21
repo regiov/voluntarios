eb0be49780eb461ed35b0cce04b86c494a1ac26c
regiov <renato@cria.org.br>
2024-04-21 18:48:57 -0300

    Nova interface adm para processos seletivos

--- vol/admin.py
+++ vol/admin.py
@@ -21,7 +21,7 @@ from tinymce.widgets import TinyMCE
 
 from mptt.admin import DraggableMPTTAdmin, TreeRelatedFieldListFilter
 
-from vol.models import Usuario, AreaTrabalho, AreaAtuacao, Voluntario, Entidade, VinculoEntidade, Necessidade, AreaInteresse, AnotacaoEntidade, TipoDocumento, Documento, Telefone, Email, FraseMotivacional, ForcaTarefa, Conteudo, AcessoAConteudo, TipoArtigo, NecessidadeArtigo, Funcao, PostagemBlog, TermoAdesao
+from vol.models import Usuario, AreaTrabalho, AreaAtuacao, Voluntario, Entidade, VinculoEntidade, Necessidade, AreaInteresse, AnotacaoEntidade, TipoDocumento, Documento, Telefone, Email, FraseMotivacional, ForcaTarefa, Conteudo, AcessoAConteudo, TipoArtigo, NecessidadeArtigo, Funcao, PostagemBlog, TermoAdesao, ProcessoSeletivo
 
 from notification.models import Message
 from notification.utils import notify_user_msg
@@ -1010,6 +1010,17 @@ class TermoAdesaoAdmin(admin.ModelAdmin):
         main_msg = u'%s usuário(s) notificado(s). ' % num_messages
         self.message_user(request, "%s" % (main_msg))
     reenviar_para_voluntario.short_description = "Reenviar mensagem para voluntário"
+
+@admin.register(ProcessoSeletivo)
+class ProcessoSeletivoAdmin(admin.ModelAdmin):
+    '''Interface administrativa para processos seletivos'''
+    list_display = ('titulo', 'entidade', 'inicio_inscricoes', 'limite_inscricoes', 'nome_status',)
+    fields = ['codigo', 'titulo', 'entidade', 'cadastrado_por', 'cadastrado_em', 'resumo_entidade', 'modo_trabalho', 'estado', 'cidade', 'atividades', 'carga_horaria', 'requisitos', 'inicio_inscricoes', 'limite_inscricoes', 'previsao_resultado', 'qtde_visualiza']
+    readonly_fields = ['codigo', 'cadastrado_por', 'cadastrado_em', 'entidade', 'estado', 'cidade', 'inicio_inscricoes', 'limite_inscricoes', 'previsao_resultado', 'qtde_visualiza']
+    
+    # Desabilita inclusão
+    def has_add_permission(self, request):
+        return False
 
 @admin.register(Funcao)
 class FuncaoAdmin(DraggableMPTTAdmin):
