bce7913cb79a54ab9cff31d5e5c351c9a3820f51
regiov <renato@cria.org.br>
2025-03-15 08:49:41 -0300

    Correção em condição para envio de notificação automática de novo processo seletivo

--- vol/utils.py
+++ vol/utils.py
@@ -140,7 +140,7 @@ def notifica_aprovacao_entidade(entidade):
 
 def notifica_processo_seletivo_aguardando_aprovacao(processo_seletivo):
     '''Envia e-mail comunicando ao RH a existência de processo seletivo aguardando aprovação'''
-    if hasattr(settings, 'RH_TEAM_EMAIL'):
+    if hasattr(settings, 'RH_TEAM_EMAIL') and settings.RH_TEAM_EMAIL and settings.RH_TEAM_EMAIL != 'SET IN LOCAL SETTINGS':
         notify_email(settings.RH_TEAM_EMAIL, '\o/ Novo processo seletivo!', "Ei! Tem um novo processo seletivo aguardando aprovação:\n\nEntidade: " + processo_seletivo.entidade.menor_nome() + "\nVaga: " + processo_seletivo.titulo + "\n\nVocê está recebendo este e-mail porque faz parte da equipe de RH. Assim que puder, acesse o painel de controle para fazer a revisão: https://voluntarios.com.br" + reverse('revisao_processos_seletivos'))
 
 def monta_query_string(request, excluir=['page', 'pp', 'csrfmiddlewaretoken']):
